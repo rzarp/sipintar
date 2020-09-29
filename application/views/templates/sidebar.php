5dbb1ef611502d4961e61188ad8b596a44b7953c
wafiputra <wafi.putra.arya@gmail.com>
2020-09-30 01:33:02 +0700

    sixth commit

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -65,9 +65,9 @@
            </div>
 
            <li class="nav-item active">
-               <a class="nav-link" href="<?php echo base_url('login/logout'); ?> " data-toggle="modal" data-target="#logoutModal">
+               <a class="nav-link" href="<?php echo base_url('login/logout'); ?>">
                    <i class="fas fa-fw fa fa-key"></i>
-                   <span >Logout</span>
+                   <span>Logout</span>
                </a>
            </li>
 
@@ -78,23 +78,3 @@
 
        </ul>
        <!-- End of Sidebar -->
-
-
-       <!-- Logout Modal-->
-        <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Logout</h5>
-                        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">×</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">Apakah anda yakin untuk keluar ?</div>
-                    <div class="modal-footer">
-                        <button class="btn btn-secondary" type="button" data-dismiss="modal">Batal</button>
-                        <a class="btn btn-danger" href="<?php echo base_url('login/logout'); ?>">Logout</a>
-                    </div>
-                </div>
-            </div>
-        </div>
